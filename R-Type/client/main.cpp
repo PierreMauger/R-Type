06ab914d30d5650e71ee2f5a3ae73c047dbf0ac1
Dorian Ayoul <dorian.ayoul@epitech.eu>
2022-09-29 12:36:48 +0200

    feat(RT-3) try SFML version of cmake

--- R-Type/client/main.cpp
+++ R-Type/client/main.cpp
@@ -1,9 +1,23 @@
-#include "ECS/Engine.hpp"
-#include "Includes.hpp"
+// #include "ECS/Engine.hpp"
+// #include "Includes.hpp"
+#include <SFML/Graphics.hpp>
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
 {
-    ECS::Engine engine;
-    
-    return 0;
+    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
+    window.setFramerateLimit(144);
+
+    while (window.isOpen())
+    {
+        for (auto event = sf::Event{}; window.pollEvent(event);)
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
 }
