164e769e34ce01cb8b376afbfd94efc788597ed8
NairodGH <dorian.ayoul@epitech.eu>
2022-10-04 14:47:56 +0200

    fix(RT-14): remove useless cmake command

--- r-type.bat
+++ r-type.bat
@@ -3,8 +3,5 @@
 if not exist build (
     mkdir build
 )
-cd build
-cmake ..
-cd ..
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build -j 8
--- r-type.sh
+++ r-type.sh
@@ -3,8 +3,5 @@
 if [ ! -d "build" ]; then
     mkdir build
 fi
-cd build
-cmake ..
-cd ..
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Debug
 cmake --build build -j 8
