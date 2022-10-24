72bd0d6d3d2faf016ef33cc1d1d96d163938b878
pierre.hamel <pierre1754@outlook.com>
2022-10-24 15:21:55 +0200

    fix(main): server windows size

--- R-Type/Engine/Graphic/Graphic.cpp
+++ R-Type/Engine/Graphic/Graphic.cpp
@@ -4,15 +4,16 @@ using namespace eng;
 
 Graphic::Graphic()
 {
-
     this->_window = std::make_shared<sf::RenderWindow>(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "R-Type");
+    this->_screenSize = std::make_shared<sf::Vector2f>(this->_window->getSize().x, this->_window->getSize().x);
     this->_event = std::make_shared<sf::Event>();
     this->_clock = std::make_shared<sf::Clock>();
     this->_window->setFramerateLimit(60);
     this->_window->setKeyRepeatEnabled(true);
     while (this->_window->pollEvent(*this->_event)) {
         if (this->_event->type == sf::Event::Resized) {
-            this->_screenSize = std::make_shared<sf::Vector2f>(this->_event->size.width, this->_event->size.height);
+            this->_screenSize->x = this->_event->size.width;
+            this->_screenSize->y = this->_event->size.height;
             this->_lastSize = sf::Vector2f(this->_event->size.width, this->_event->size.height);
         }
     }
