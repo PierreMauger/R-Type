0f4ddabe0861be8009e07a60e6d7e9cabb5414eb
PierreMauger <pierre.mauger@epitech.eu>
2022-11-13 16:03:13 +0100

    feat(RT-53-ButtonState): button color

--- R-Type/Engine/ECS/System/Click/ClickSystem.cpp
+++ R-Type/Engine/ECS/System/Click/ClickSystem.cpp
@@ -24,23 +24,26 @@ void ClickSystem::update(ComponentManager &componentManager, EntityManager &enti
 {
     unsigned long int changed = -1;
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-        for (auto id : entityManager.getMaskCategory(this->_buttonTag)) {
-            Button &button = componentManager.getSingleComponent<Button>(id);
-            Position pos = componentManager.getSingleComponent<Position>(id);
-            Size size = componentManager.getSingleComponent<Size>(id);
+    for (auto id : entityManager.getMaskCategory(this->_buttonTag)) {
+        Button &button = componentManager.getSingleComponent<Button>(id);
+        Position pos = componentManager.getSingleComponent<Position>(id);
+        Size size = componentManager.getSingleComponent<Size>(id);
+        SpriteAttribut &spriteAt = componentManager.getSingleComponent<SpriteAttribut>(id);
 
-            if (entityManager.hasMask(id, this->_sceneTag)) {
-                Scene &scene = componentManager.getSingleComponent<Scene>(id);
-                if (scene.id != *this->_sceneId)
-                    continue;
+        if (entityManager.hasMask(id, this->_sceneTag)) {
+            Scene scene = componentManager.getSingleComponent<Scene>(id);
+            if (scene.id != *this->_sceneId)
+                continue;
+        }
+        sf::Vector2i mousePos = sf::Mouse::getPosition(*this->_window);
+
+        if (mousePos.x >= pos.x && mousePos.x <= pos.x + size.x && mousePos.y >= pos.y && mousePos.y <= pos.y + size.y) {
+            if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+                spriteAt.color = sf::Color(128, 128, 128, 255);
+                changed = id;
             }
-            sf::Vector2i mousePos = sf::Mouse::getPosition(*this->_window);
-
-            if (mousePos.x >= pos.x && mousePos.x <= pos.x + size.x && mousePos.y >= pos.y && mousePos.y <= pos.y + size.y) {
+            if (this->_event->type == sf::Event::MouseButtonReleased && this->_event->mouseButton.button == sf::Mouse::Left) {
                 if (button.type == ButtonType::PLAY_SOLO) {
-                    if (*this->_sceneId != SceneType::MENU)
-                        continue;
                     *this->_sceneId = SceneType::GAME;
                     *this->_isLocal = true;
                     VesselPreload::preload(this->_window->getSize(), this->_screenSize, entityManager, componentManager, this->_syncId, *this->_syncId % 4);
@@ -50,30 +53,27 @@ void ClickSystem::update(ComponentManager &componentManager, EntityManager &enti
                     *this->_isReady = true;
                 } else if (button.type == ButtonType::TEXTZONE) {
                     button.selected = true;
-                    changed = id;
                 } else if (button.type == ButtonType::CONNECT) {
-                    if (*this->_sceneId != SceneType::MENU)
-                        continue;
                     Parent parent = componentManager.getSingleComponent<Parent>(id);
                     std::string text = componentManager.getSingleComponent<Text>(parent.id).str;
                     std::string text2 = componentManager.getSingleComponent<Text>(parent.id2).str;
                     *this->_ip = text;
                     text2 != "" ? *this->_port = std::stoi(text2) : *this->_port = 0;
                 } else if (button.type == ButtonType::CREATE_ROOM) {
-                    if (*this->_sceneId != SceneType::LOBBY)
-                        continue;
                     Parent parent = componentManager.getSingleComponent<Parent>(id);
                     std::string text = componentManager.getSingleComponent<Text>(parent.id).str;
                     text != "" ? *this->_roomPlayerMax = std::stoi(text) : *this->_roomPlayerMax = 4;
                 }
             }
-            if (changed) {
-                for (auto id : entityManager.getMaskCategory(this->_buttonTag)) {
-                    Button &button = componentManager.getSingleComponent<Button>(id);
-                    if (button.type == ButtonType::TEXTZONE && id != changed)
-                        button.selected = false;
-                }
-            }
+        }
+    }
+    for (auto id : entityManager.getMaskCategory(this->_buttonTag)) {
+        Button &button = componentManager.getSingleComponent<Button>(id);
+        SpriteAttribut &spriteAt = componentManager.getSingleComponent<SpriteAttribut>(id);
+        if (id != changed) {
+            if (button.type == ButtonType::TEXTZONE && sf::Mouse::isButtonPressed(sf::Mouse::Left))
+                button.selected = false;
+            spriteAt.color = sf::Color::White;
         }
     }
 }
